c61bd1e422f919c66ed76b4fa4212628f872bb6e
rahulkr-dev <rkrahul8181@gmail.com>
2026-01-09 14:51:32 +0530

    feat: update trustedOrigins to dynamically fetch from reseller_app collection

--- apps/auth/lib/auth.ts
+++ apps/auth/lib/auth.ts
@@ -10,7 +10,31 @@ import { getDatabase } from "./mongodb"
 const from = process.env.DEFAULT_FROM_EMAIL || "no-reply.Veblika.com"
 
 export const auth: ReturnType<typeof betterAuth> = betterAuth({
-  trustedOrigins: ["https://*.backendassist.com", "http://localhost:*"],
+  trustedOrigins: async (request) => {
+    if (!request) {
+      return ["https://*.backendassist.com", "http://localhost:*"]
+    }
+
+    // Get all host entries from reseller_app collection
+    const resellerApps = await getDatabase()
+      .db.collection("reseller_app")
+      .find({}, { host: 1, _id: 0 })
+      .toArray()
+
+    // Here use Set to avoid duplicate origins
+    // Todo : 1 ) Cache this result to avoid DB query on every request
+    // 2 ) Validate the origin
+    const originsSet = new Set<string>()
+    resellerApps.forEach((app: { host: string }) => {
+      originsSet.add("https://*.backendassist.com")
+      originsSet.add("http://localhost:*")
+      if (app?.host) {
+        originsSet.add(`https://${app.host}`)
+      }
+    })
+    console.log("Trusted origins:", Array.from(originsSet))
+    return Array.from(originsSet)
+  },
   // advanced: {
   //   crossSubDomainCookies: {
   //           enabled: true,
@@ -23,8 +47,6 @@ export const auth: ReturnType<typeof betterAuth> = betterAuth({
   advanced: {
     crossSubDomainCookies: {
       enabled: true,
-      domain:
-        process.env.NODE_ENV === "production" ? "backendassist.com" : undefined,
     },
     // defaultCookieAttributes: {
     //   secure: true,
